a430ca232733a60a0eefa8c35115080c38dc0ef6
Andrew Garcia <andrew@drwgrc.dev>
2022-03-05 14:05:21 -0500

    added development and production scripts, added launches route and model

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = require('./app');
 
-const { loadPlanetsData } = require('./models/planets.models');
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
