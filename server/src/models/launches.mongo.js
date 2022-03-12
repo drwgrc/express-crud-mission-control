07f5b8d865ef57d01597d90be263c55ca2cf5cf5
Andrew Garcia <andrew@drwgrc.dev>
2022-03-11 20:33:12 -0500

    added pm2 for cluster management, connected to mongodb using mongoose, created launches and planets mongoose schema

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: {
+    type: Array,
+    required: false,
+  },
+  upcoming: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model('Launch', launchesSchema);
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,21 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  'mongodb+srv://nasa-api:1hUAXItia4pc4NSL@cluster0.0pahg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready');
+});
+
+mongoose.connection.on('error', err => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
