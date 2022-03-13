ae1a56f5ceb72e149b3f010aa5c20dd0019e4000
Andrew Garcia <andrew@drwgrc.dev>
2022-03-12 19:54:48 -0500

    added destination planets from mongodb

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const parse = require('csv-parse');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 const isHabitable = planet => {
   return (
@@ -19,24 +19,43 @@ function loadPlanetsData() {
       path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
     )
       .pipe(parse({ comment: '#', columns: true }))
-      .on('data', data => {
+      .on('data', async data => {
         if (isHabitable(data)) {
-          habitablePlanets.push(data);
+          await savePlanet(data);
         }
       })
       .on('error', err => {
         console.log(err);
         reject(err);
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on('end', async () => {
+        const countPlentsFound = (await getAllPlanets()).length;
+        console.log(`${countPlentsFound} habitable planets found!`);
         resolve();
       });
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({});
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      }
+    );
+  } catch (err) {
+    console.error(`Could not save a planet - ${err}`);
+  }
 }
 
 module.exports = {
